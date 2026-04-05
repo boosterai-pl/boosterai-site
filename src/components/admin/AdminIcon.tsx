a9213001837d1a74837015a35721f521922184cc
Szymon Sidor <szsidor@gmail.com>
2026-04-05 23:51:16 +0200

    feat(admin): add AdminIcon component

---
+++ src/components/admin/AdminIcon.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const AdminIcon: React.FC = () => (
+  <img
+    src="/Blue-sygnet-with-background-1x1-1.png"
+    alt="Booster AI"
+    style={{ height: '32px', width: '32px', objectFit: 'contain', borderRadius: '6px' }}
+  />
+)
+
+export default AdminIcon
