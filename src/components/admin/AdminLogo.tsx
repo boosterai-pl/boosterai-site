af88ec8021353773812505037bbbc8510fd45d83
Szymon Sidor <szsidor@gmail.com>
2026-04-05 23:51:12 +0200

    feat(admin): add AdminLogo component

---
+++ src/components/admin/AdminLogo.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const AdminLogo: React.FC = () => (
+  <img
+    src="/Booster-logo.png"
+    alt="Booster AI"
+    style={{ height: '28px', width: 'auto', objectFit: 'contain' }}
+  />
+)
+
+export default AdminLogo
